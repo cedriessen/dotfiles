909fa0407082793f4f801ff35c46aecd3f66e547
Christoph Drießen <cdriessen@extron.com>
2017-09-14 13:52:47 +0200

    add formulae to brew setup

--- install/brew.sh
+++ install/brew.sh
@@ -2,21 +2,13 @@
 
 # Installs Homebrew and some of the common dependencies needed/desired for software development
 
-# Ask for the administrator password upfront
-sudo -v
-
 # Check for Homebrew and install it if missing
-if test ! $(which brew)
-then
+if test ! $(which brew); then
   echo "Installing Homebrew..."
   ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
-brew tap homebrew/core
-brew tap homebrew/dupes
-brew tap homebrew/science
 brew tap caskroom/cask
-brew tap jenv/jenv
 
 # Make sure we’re using the latest Homebrew
 brew update
@@ -25,40 +17,52 @@ brew update
 brew upgrade --all
 
 apps=(
-  git
+  activemq
+  bnd
+  ccrypt
+  curl
   docker
   docker-compose
   docker-machine
   elixir
-  htop
-  activemq
-  zsh
-  zplug
   ffmpeg
+  git
+  htop
+  maven
+  sbt
+  scala
+  smartmontools
+  typesafe-activator
   wget
-  curl
-  ccrypt
+  zplug
+  zsh
+
 )
 
 brew install "${apps[@]}"
 
 osx_apps=(
+  java
   virtualbox
+  jetbrains-toolbox
+  evernote
+  1password
+  spectacle
+
   # QuickLook plugins
-  qlcolorcode
-  qlstephen
-  qlmarkdown
-  quicklook-json
-  qlprettypatch
-  quicklook-csv
   betterzipql
+  qlcolorcode
   qlimagesize
-  webpquicklook
+  qlmarkdown
+  qlprettypatch
+  qlstephen
+  quicklook-csv
+  quicklook-json
   suspicious-package
+  webpquicklook
 )
 
 brew cask install "${osx_apps[@]}"
-
 
 # Remove outdated versions from the cellar
 brew cleanup
